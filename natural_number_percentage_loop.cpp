f639ad309407adf0e2bd2625848a6d7c1fcff80b
Martin Bångens <noisecode3@gmail.com>
2024-07-05 20:01:18 +0200

    remove scale solution

--- natural_number_percentage_loop.cpp
+++ natural_number_percentage_loop.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
-int main()
-{
+
+int main() {
     unsigned int n; // Number of iterations
     unsigned int gotoPercent; // Percentage of total work
-    unsigned int scale = 1000; // Resolution scale
+
     std::cout << "Number of iterations: ";
     std::cin >> n;
 
-    if (n <= 0 || n > scale)
-    {
+    if (n <= 0) {
         std::cout << "Invalid input for number of iterations\n";
         return 1;
     }
@@ -16,57 +15,35 @@ int main()
     std::cout << "Percentage of total work 1 to 100: ";
     std::cin >> gotoPercent;
 
-    if (gotoPercent < 1 || gotoPercent > 100)
-    {
+    if (gotoPercent < 1 || gotoPercent > 100) {
         std::cout << "Invalid percentage value\n";
         return 1;
     }
 
-    unsigned int progress = 0; // Current progress
     unsigned int lastPrintedPercent = 0; // Last printed percentage
 
-    // Calculate progress increment per iteration
-    unsigned int progressIncrement = scale / n;
-
-    // Calculate remainder
-    unsigned int remainder = scale % n;
-
     // Perform iterations
-    for (unsigned int i = 0; i < n; ++i)
-    {
-        // Increment progress by progress increment
-        progress += progressIncrement;
-
-        // Adjust progress if remainder is non-zero
-        if (remainder > 0)
-        {
-            ++progress;
-            --remainder;
-        }
-
-        // Scale progress to match the resolution we need
-        unsigned int scaledProgress = progress * gotoPercent / scale;
+    for (unsigned int i = 0; i < n; ++i) {
+        // Calculate the current percentage using integer arithmetic
+        unsigned int currentPercent = (i + 1) * gotoPercent / n;
 
         // Print only if the percentage has changed
-        if (scaledProgress != lastPrintedPercent)
-        {
+        if (currentPercent != lastPrintedPercent) {
             // Output progress bar
             std::cout << "[";
 
             // Output progress bar characters
-            for (unsigned int j = 0; j < scaledProgress; ++j)
-            {
+            for (unsigned int j = 0; j < currentPercent; ++j) {
                 std::cout << "=";
             }
-            for (unsigned int j = scaledProgress; j < gotoPercent; ++j)
-            {
+            for (unsigned int j = currentPercent; j < gotoPercent; ++j) {
                 std::cout << " ";
             }
 
-            std::cout << "] " << scaledProgress << "%\n";
+            std::cout << "] " << currentPercent << "%\n";
 
             // Update last printed percentage
-            lastPrintedPercent = scaledProgress;
+            lastPrintedPercent = currentPercent;
         }
     }
     return 0;
